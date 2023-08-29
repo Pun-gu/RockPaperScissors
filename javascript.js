14db3589df595bbc30287261129be4ddc2a255d1
Pun-gu <132360422+Pun-gu@users.noreply.github.com>
2023-08-29 17:23:19 +0900

    Added function that checks who wins, and loop comm and.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,49 @@
-function getComputerChoice(){
-let randomNumber;
-randomNumber = Math.floor(Math.random()*3)+1; //This makes comp generate number between 1-3//
-switch (randomNumber) {
+
+    function getComputerChoice(){
+let randomNum = Math.floor(Math.random()*3)+1; //This makes comp generate number between 1-3//
+switch (randomNum) {
     case 1:
-        return('Rock');
+        return('rock');
         break;
     case 2:
-        return('Paper');
+        return('paper');
         break; 
     case 3:
-        return('Scissors');
+        return('scissors');
 }} //Depending on the number generated, it spits out R, P, S// 
 
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        console.log("It is a tie")
+    }else if(playerSelection == 'rock'){
+        if(computerSelection == 'scissors'){
+            console.log('playerWins')
+        }else{
+            console.log('playerLoses')
+        }
+    }else if(playerSelection == 'paper'){
+        if(computerSelection == 'rock'){
+            console.log('PlayerWins')
+        }else{
+            console.log('PlayerLoses')
+        }
+    }else if(playerSelection == "scissors"){
+        if(computerSelection == 'paper'){
+            console.log('PlayerWins')
+        }else{
+            console.log('PlayerLoses')
+        }
+    }
+} // This code checks the who is winner of R,P,S//
 
-function playRound (playerSelection, computerSelection){
-    const playerSelection = 'ROCK'
-    const computerSelection = getComputerChoice();
+function game(){
+    let playerSelection;
+    let computerSelection;
+    for(let i = 0; i<5 ; i++){
+        let playerSelection = prompt('Rock, Paper, Scissors');
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection);
 }
-
+}
+game();
