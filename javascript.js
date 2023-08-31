87ac36262d9e0229b3d16b1128f9fee30179cd55
Pun-gu <132360422+Pun-gu@users.noreply.github.com>
2023-08-31 20:07:54 +0900

    Added code to keep score of comp and player

--- javascript.js
+++ javascript.js
@@ -18,32 +18,49 @@ function playRound(playerSelection, computerSelection){
         console.log("It is a tie")
     }else if(playerSelection == 'rock'){
         if(computerSelection == 'scissors'){
-            console.log('playerWins')
+            console.log('Player Wins Round')
         }else{
-            console.log('playerLoses')
+            console.log('Computer Wins Round')
         }
     }else if(playerSelection == 'paper'){
         if(computerSelection == 'rock'){
-            console.log('PlayerWins')
+            console.log('Player Wins Round')
         }else{
-            console.log('PlayerLoses')
+            console.log('Computer Wins Round')
         }
     }else if(playerSelection == "scissors"){
         if(computerSelection == 'paper'){
-            console.log('PlayerWins')
+            console.log('Player Wins Round')
         }else{
-            console.log('PlayerLoses')
+            console.log('Computer Wins Round')
         }
     }
 } // This code checks the who is winner of R,P,S//
 
+let playerScore = 0; //Variables to hold score for player and computer//
+let computerScore = 0;
 function game(){
     let playerSelection;
     let computerSelection;
     for(let i = 0; i<5 ; i++){
-        let playerSelection = prompt('Rock, Paper, Scissors');
+        let playerSelection = prompt('Rock, Paper, Scissors').toLowerCase();
         let computerSelection = getComputerChoice();
         playRound(playerSelection,computerSelection);
-}
-}
+        if(playerSelection ==  computerSelection){
+            console.log(`It is a Tie! Player Score is ${playerScore},Computer Score is ${computerScore}`);
+        }else if(playerSelection == 'rock'&& computerSelection=='scissors'){
+            playerScore++;
+            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+        }else if(playerSelection == 'scissors'&& computerSelection=='paper'){
+            playerScore++;
+            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+        }else if(playerSelection == 'paper'&& computerSelection=='rock'){
+            playerScore++;
+            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+        }else{
+            computerScore++;
+            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+        }
+}}
+//this runs all the functions and also keeps track of the scores and outputs the scores in console//
 game();
