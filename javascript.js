7ba91fafd4235535c48892005802863f66319cc5
Pun-gu <132360422+Pun-gu@users.noreply.github.com>
2023-10-03 16:11:42 +0900

    Added UI for interaction

--- javascript.js
+++ javascript.js
@@ -1,66 +1,84 @@
 
-    function getComputerChoice(){
-let randomNum = Math.floor(Math.random()*3)+1; //This makes comp generate number between 1-3//
-switch (randomNum) {
-    case 1:
-        return('rock');
-        break;
-    case 2:
-        return('paper');
-        break; 
-    case 3:
-        return('scissors');
-}} //Depending on the number generated, it spits out R, P, S// 
-
-
-function playRound(playerSelection, computerSelection){
-    if(playerSelection == computerSelection){
-        console.log("It is a tie")
-    }else if(playerSelection == 'rock'){
-        if(computerSelection == 'scissors'){
-            console.log('Player Wins Round')
-        }else{
-            console.log('Computer Wins Round')
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random()*3)+1; //This makes comp generate number between 1-3//
+    switch (randomNum) {
+        case 1:
+            return('rock');
+            break;
+        case 2:
+            return('paper');
+            break; 
+        case 3:
+            return('scissors');
+    }} //Depending on the number generated, it spits out R, P, S// 
+    
+    function playRound(playerSelection, computerSelection){
+        if(playerSelection == computerSelection){
+            result = "It is a tie";
+        }else if(playerSelection == 'rock'){
+            if(computerSelection == 'scissors'){
+                playerScore ++;
+                result = 'Player Wins ';
+            }else{
+                computerScore ++;
+                result = 'Computer Wins ';
+            }
+        }else if(playerSelection == 'paper'){
+            if(computerSelection == 'rock'){
+                playerScore ++;
+                result = 'Player Wins ';
+            }else{
+                computerScore ++;
+                result = 'Computer Wins ';
+            }
+        }else if(playerSelection == "scissors"){
+            if(computerSelection == 'paper'){
+                playerScore ++;
+                result = 'Player Wins ';
+            }else{
+                computerScore++;
+                result = 'Computer Wins ';
+            }
         }
-    }else if(playerSelection == 'paper'){
-        if(computerSelection == 'rock'){
-            console.log('Player Wins Round')
-        }else{
-            console.log('Computer Wins Round')
-        }
-    }else if(playerSelection == "scissors"){
-        if(computerSelection == 'paper'){
-            console.log('Player Wins Round')
-        }else{
-            console.log('Computer Wins Round')
-        }
-    }
-} // This code checks the who is winner of R,P,S//
-
-let playerScore = 0; //Variables to hold score for player and computer//
+    } 
+let selectionButtons = document.querySelectorAll(".playerSelect");
+let playerText = document.querySelector("#playerText");
+let computerText = document.querySelector("#computerText")
+let resultText = document.querySelector(".results")
+let playerSelection;
+let computerSelection;    
+let playerScore = 0;
 let computerScore = 0;
-function game(){
-    let playerSelection;
-    let computerSelection;
-    for(let i = 0; i<5 ; i++){
-        let playerSelection = prompt('Rock, Paper, Scissors').toLowerCase();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
-        if(playerSelection ==  computerSelection){
-            console.log(`It is a Tie! Player Score is ${playerScore},Computer Score is ${computerScore}`);
-        }else if(playerSelection == 'rock'&& computerSelection=='scissors'){
-            playerScore++;
-            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
-        }else if(playerSelection == 'scissors'&& computerSelection=='paper'){
-            playerScore++;
-            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
-        }else if(playerSelection == 'paper'&& computerSelection=='rock'){
-            playerScore++;
-            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
-        }else{
-            computerScore++;
-            console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
-        }
-}}
-//this runs all the functions and also keeps track of the scores and outputs the scores in console//
-game();
+let result;
+
+
+selectionButtons.forEach(selectedButton => {
+    selectedButton.addEventListener('click', e => {
+        const playerSelection = selectedButton.dataset.selection
+        const computerSelection = getComputerChoice();
+        playerText.textContent = `Player: ${playerScore}`
+        computerText.textContent = `Computer: ${computerScore}`
+        resultText.textContent = `Result: ${result}`
+        playRound(playerSelection, computerSelection)
+    })
+})
+
+
+    // function game(){
+    //     playRound(playerSelection,computerSelection);
+    //     if(playerSelection ==  computerSelection){
+    //         console.log(`It is a Tie! Player Score is ${playerScore},Computer Score is ${computerScore}`);
+    //     }else if(playerSelection == 'rock'&& computerSelection=='scissors'){
+    //         playerScore++;
+    //         console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+    //     }else if(playerSelection == 'scissors'&& computerSelection=='paper'){
+    //         playerScore++;
+    //         console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+    //     }else if(playerSelection == 'paper'&& computerSelection=='rock'){
+    //         playerScore++;
+    //         console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+    //     }else{
+    //         computerScore++;
+    //         console.log(`Player Score is ${playerScore},Computer Score is ${computerScore}`);
+    //     }
+    // }
